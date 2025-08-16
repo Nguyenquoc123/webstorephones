f9cecf8b3d8b82e1a4923e0257b5d2abe3bbbbab
van sang <sangng0166@gmail.com>
2025-08-17 02:53:20 +0700

    add fuc ho so ca nhan

--- frontend_react/storephones/src/App.js
+++ frontend_react/storephones/src/App.js
@@ -1,36 +1,27 @@
-import "./App.css";
-import logo from "./logo.jpg"; // hoặc logo.svg, tuỳ bạn chọn file muốn dùng
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import Login from "./compenent/login/Login";
-import Signup from "./compenent/signup/Signup";
-import ChiTietDienThoai from "./compenent/chitietdienthoai/ChiTietDienThoai";
-import DienThoai from "./compenent/quanlysanpham/dienthoai";
-import HomeKhachHang from "./compenent/homekhachhang/HomeKhachHang";
-import DanhMuc from "./compenent/quanlydanhmuc/DanhMuc";
-import Admin from "./compenent/Admin/Admin";
-import DoiMatKhau from "./compenent/DoiMatKhau/DoiMatKhau";
-import GioHang from "./compenent/giohang/Giohang";
-import DonHang from "./compenent/quanlydonhang/DonHang";
-import ChiTietDonHang from "./compenent/quanlydonhang/chitietdonhang/PageChiTietDonHang";
+import './App.css';
+import Login from './compenent/login/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Signup from './compenent/signup/Signup';
+import ChiTietDienThoai from './compenent/chitietdienthoai/ChiTietDienThoai';
+import DienThoai from './compenent/quanlysanpham/dienthoai'
+import HomeKhachHang from './compenent/homekhachhang/HomeKhachHang';
+import DanhMuc from './compenent/quanlydanhmuc/DanhMuc';
+import Giohang from './compenent/giohang/Giohang';
+import HoSoCaNhan from './compenent/hosocanhan/HoSoCaNhan';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/dienthoai" element={<DienThoai />} />
-        <Route path="/danhmuc" element={<DanhMuc />} />
-        <Route path="/home/:makhachhang" element={<HomeKhachHang />} />
-        <Route path="/chitietdienthoai/:value" element={<ChiTietDienThoai />} />
-        <Route path="/Admin" element={<Admin />} />
-        <Route path="/doimatkhau" element={<DoiMatKhau />} />
-        <Route path="/home" element={<HomeKhachHang />} />
-        <Route path="/home/giohang" element={<GioHang />} />
-        <Route path="/donhang" element={<DonHang />} />
-        <Route path="/chitietdonhang/:maDonHang" element={<ChiTietDonHang />} />
+        <Route path='/' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/dienthoai' element={<DienThoai />} />
+        <Route path='/danhmuc' element={<DanhMuc />} />
+        <Route path='/home' element={<HomeKhachHang />} />
+        <Route path='/home/hosocanhan' element={<HoSoCaNhan />} />
+        <Route path='/home/giohang' element={<Giohang />} />
+        <Route path='/chitietdienthoai/:value' element={<ChiTietDienThoai />} />
       </Routes>
     </BrowserRouter>
   );
