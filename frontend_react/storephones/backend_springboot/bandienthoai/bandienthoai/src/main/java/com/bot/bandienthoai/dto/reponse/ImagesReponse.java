1d9ae1a15ed36660b9257a7ac1f0173e53657deb
nguyễnquoc123 <quoc4373@gmail.com>
2025-08-25 23:27:46 +0700

    update folder

--- frontend_react/storephones/backend_springboot/bandienthoai/bandienthoai/src/main/java/com/bot/bandienthoai/dto/reponse/ImagesReponse.java
+++
@@ -1,9 +0,0 @@
-package com.bot.bandienthoai.dto.reponse;
-
-import lombok.Data;
-
-@Data
-public class ImagesReponse {
-	private Integer id;
-	private String url;
-}
--- frontend_react/storephones/backend_springboot/bandienthoai/bandienthoai/src/main/java/com/bot/bandienthoai/request/CartItemRequest.java
+++
@@ -1,9 +0,0 @@
-package com.bot.bandienthoai.request;
-
-import lombok.Data;
-
-@Data
-public class CartItemRequest {
-	private Integer maPhienBan;
-	private Integer soLuong;
-}
